7c29db0d71d82ca7a8fb18ebe3047603f57eca2b
hackerstore <106470800+hackerstore@users.noreply.github.com>
2023-07-10 17:50:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function redirectUser() {
     // Perform validation and redirect logic here
     // Example logic: If ID key is "123", redirect to user's page
     if (idInput === "ADMIN@SN1234") {
-      window.location.href = "https://hackerstore.github.io/"; // Replace with the URL of the user's page
+      window.location.href = "https://databaseofhid.vercel.app/hjhhdjhsjhddkjasjhhhdjhsjh/"; // Replace with the URL of the user's page
     } else {
       alert("Invalid ID key. Please try again."); // Show an error message
     }
