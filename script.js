054d443f16074e6e4bf385a9fd64304438c977c4
hackerstore <106470800+hackerstore@users.noreply.github.com>
2023-07-10 17:46:11 +0530

    as

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function redirectUser() {
     
     // Perform validation and redirect logic here
     // Example logic: If ID key is "123", redirect to user's page
-    if (idInput === "123") {
+    if (idInput === "702560480412") {
       window.location.href = "https://hackerstore.github.io/"; // Replace with the URL of the user's page
     } else {
       alert("Invalid ID key. Please try again."); // Show an error message
